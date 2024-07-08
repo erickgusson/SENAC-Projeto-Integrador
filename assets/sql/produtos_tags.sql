9d9c5754856cb94e521d9f71bead9c11a9bbc74b
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-07-07 22:53:41 -0300

    Tags para relacionamento de prodotos (Arquivo temporário)
    Arquivo feito para fazer a relação de tags e produtos
    Remoção no futuro quando concluido o sistema de cadastro e edição de produtos

---
+++ assets/sql/produtos_tags.sql
@@ -0,0 +1,165 @@
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(1, 2);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(1, 6);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(2, 2);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(2, 6);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(3, 2);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(3, 6);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(4, 4);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(4, 8);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(4, 10);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(4, 11);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(5, 7);
+
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(6, 7);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(7, 7);
+
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(8, 7);
+
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(9, 3);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(9, 6);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(10, 3);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(10, 6);
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(11, 6);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(11, 10);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(12, 5);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(12, 4);
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(12, 8);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(13, 0);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(14, 0);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(15, 0);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(16, 0);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(17, 0);
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(18, 0);
+
+
+
+
+
+
+INSERT INTO `tb_produtos_tags`(`id_produto`, `id_tag`) 
+VALUES(1, 6);
