12557a9da991d8d423452428a5a2d53a1012c0f6
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-07-07 22:01:55 -0300

    Inserção das tags

---
+++ assets/sql/tags.sql
@@ -0,0 +1,32 @@
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Torta');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Bolo');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Cookie');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Diet');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Mousse');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Massas');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Cones');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('De frutas');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Sorvetes');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Docinhos');
+
+INSERT INTO `tb_tags`(`tag`) 
+VALUES('Especiais');
