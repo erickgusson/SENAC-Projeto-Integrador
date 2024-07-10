235a64e3ca6589860086d43411bf0992c7720f9e
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-07-10 17:11:02 -0300

    Ajuste no include de produto

--- includes/produto.php
+++ includes/produto.php
@@ -1,6 +1,12 @@
-<div class="item">
-    <img src="assets/img/produtos/BoloPrestigio.png" alt="">
-    <p>Bolo de prestígio</p>
-    <p>R$ 7,50 / 100g</p>
-    <button class="adicionar">Adicionar ao Carrinho</button>
-</div>
+<?php
+
+foreach ($dados as $valor) { ?>
+
+    <a href="produto-selecionado.php?id=<?= $valor['id'] ?>" class="item">
+        <img src="assets/img/produtos/<?= $valor['imagem'] ?>.png" alt="">
+        <p><?= $valor['nome'] ?></p>
+        <p>R$ <?= $valor['preco'] ?></p>
+        <button class="adicionar">Adicionar ao Carrinho</button>
+    </a>
+
+<?php } ?>
--- produtos.php
+++ produtos.php
@@ -39,28 +39,7 @@ $dados = $produto->ListarProdutos();
             </div>
 
             <div class="lista-produtos">
-
-
-                <?php
-
-                foreach ($dados as $valor) { ?>
-
-                    <a href="produto-selecionado.php?id=<?=$valor['id']?>" class="item">
-                        <img src="assets/img/produtos/<?=$valor['imagem']?>.png" alt="">
-                        <p><?=$valor['nome']?></p>
-                        <p>R$ <?=$valor['preco']?></p>
-                        <button class="adicionar">Adicionar ao Carrinho</button>
-                    </a>
-
-                <?php }
-
-
-
-                // for ($i = 0; $i < 50; $i++) {
-                //     include "includes/produto.php";
-                // }
-
-                ?>
+                <?php include "./includes/produto.php"; ?>
             </div>
         </div>
 
